94e58a151259192030541ca048ccd96ce335f578
Peter <yuury.chang@gmail.com>
2023-10-11 09:16:55 +0000

    Add zustand devtool

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,27 @@
+import { useStore } from '@/store';
+
 export function Home() {
+  const bears = useStore.use.bears();
+  const addBear = useStore.use.addBear();
   return (
-    <h1 className="mt-8 text-xl text-center">
-      Nice to see you here! This is my react starter template. I hope you like
-      it!
-    </h1>
+    <>
+      <div className="flex justify-center">
+        <div>
+          <h1 className="mt-8 text-xl">
+            Nice to see you here! This is my react starter template. I hope you
+            like it!
+          </h1>
+          <div className="mt-8">
+            bears: {bears}
+            <button
+              className="px-3 ml-5 leading-7 text-white rounded cursor-pointer bg-neutral-400"
+              onClick={addBear}
+            >
+              add bear
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,17 @@
 import { create } from 'zustand';
 import { BearSlice, createBearSlice } from './bear';
 import { FishSlice, createFishSlice } from './fish';
+import { devtools } from 'zustand/middleware';
+import { createSelectors } from '@/utils/createSelectors';
 
-export const useStore = create<BearSlice & FishSlice>()((...a) => ({
-  ...createBearSlice(...a),
-  ...createFishSlice(...a),
-}));
+export const useStore = createSelectors(
+  create<BearSlice & FishSlice>()(
+    devtools(
+      (...a) => ({
+        ...createBearSlice(...a),
+        ...createFishSlice(...a),
+      }),
+      { enabled: import.meta.env.DEV },
+    ),
+  ),
+);
