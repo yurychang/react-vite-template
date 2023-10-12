e34d4e8cbf4d918f81cb7d9751bb7423c9a6505a
Peter <yuury.chang@gmail.com>
2023-10-12 07:26:00 +0000

    Add react query devtool

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import './index.css';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 
-import { reactQueryConfig } from './react-query-config';
-import { router } from './routes';
-
-const queryClient = new QueryClient(reactQueryConfig);
+import { App } from './app';
 
 if (import.meta.env.DEV) {
   await import('./mocks/browser').then(({ worker }) => {
@@ -18,8 +13,6 @@ if (import.meta.env.DEV) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 );
